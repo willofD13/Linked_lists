ee453d5b384dc35d5eb43760e4bf78bdf204267a
willofD13 <leonidastsiakas13@gmail.com>
2023-07-21 18:40:47 +0300

    Add an element ot the end of the list

--- main.rb
+++ main.rb
@@ -1,24 +1,32 @@
 class Node
-    attr_accessor :value, :next_node
+    attr_accessor :value, :next
 
     def initialize(value)
         @value = value
-        @next_node = nil
+        @next = nil
     end 
 end
 
 class LinkedList
     attr_accessor :head
 
-    def initialize
-        @head = nil 
+    def initialize(value)
+        @head = Node.new(value) 
     end
 
-    def prepend(value)
-        @head = Node.new(value)
+    def append(value)
+        node = head 
+
+        while node.next !=nil 
+            node = node.next 
+        end 
+
+        node.next = Node.new(value)
     end
+
 end
 
-list = LinkedList.new
-list.prepend(20)
+list = LinkedList.new(2)
+list.append(10)
+list.append(20)
 p list
