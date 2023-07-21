70c7963a64e26562549491f65897a70b5b9b6b48
willofD13 <leonidastsiakas13@gmail.com>
2023-07-21 17:41:13 +0300

    Add an element to the start of the list

--- main.rb
+++ main.rb
@@ -1,14 +1,24 @@
 class Node
-    attr_accessor :value, :next
+    attr_accessor :value, :next_node
 
     def initialize(value)
         @value = value
-        @next = nil 
+        @next_node = nil
     end 
 end
 
 class LinkedList
+    attr_accessor :head
 
-    def initialize(value)
+    def initialize
+        @head = nil 
+    end
+
+    def prepend(value)
         @head = Node.new(value)
     end
+end
+
+list = LinkedList.new
+list.prepend(20)
+p list
